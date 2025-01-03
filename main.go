3dea5ca35ea0361a9d4da3060a7e73a7b33c0231
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-03 16:11:51 +0600

    3_Jan_25|4:11PM

--- main.go
+++ main.go
@@ -2,23 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	fmt.Println("Wellcome on GO-Lang")
+	// CHI to Create Router
+	router := chi.NewRouter()
 
+	// A good base middleware stack
+	router.Use(middleware.Logger)
+	// r.Use(middleware.RequestID)
+	// r.Use(middleware.RealIP)
+	// r.Use(middleware.Recoverer)
+
+	router.Get("/hello", basicHandler)
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
-	// server running on http://localhost:3000
+	CheckError(err)
 
+}
+
+func CheckError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func basicHandler(w http.ResponseWriter, r *http.Request) {
