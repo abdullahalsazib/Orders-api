f98dc933548a1fb4cd15bae19c040177e9765b1f
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-01-05 22:15:56 +0600

    5_Jan_25|10:15Pm

--- handler/order.go
+++ handler/order.go
@@ -9,20 +9,25 @@ type Order struct{}
 
 func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Create an order...!")
+	w.Write([]byte("<h1>Create and order</h1>"))
 }
 
 func (o *Order) List(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("List all order..!")
+	w.Write([]byte("<h1>List all order</h1>"))
 }
 
 func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get an order By Id..!")
+	w.Write([]byte("<h1>Get an order by id</h1>"))
 }
 
 func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Update an order By Id")
+	w.Write([]byte("<h1>Update and order by id</h1>"))
 }
 
 func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Delete an order by id")
+	w.Write([]byte("<h1>Delete an order by id</h1>"))
 }
